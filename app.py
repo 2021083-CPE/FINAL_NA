1f6aa2eb212f2f02abc58291edcaaa1c803d1055
2021083-CPE <2021083@ub.edu.ph>
2023-11-14 08:12:55 +0800

    final

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, Response
 from ultralytics import YOLO
 import cv2
-import mysql.connector
 from flask_mysqldb import MySQL
 import json
+import time
 
 app = Flask(__name__)
 model = YOLO('finalsbest.pt')
@@ -16,22 +16,14 @@ app.config['MYSQL_DB'] = "detection"
 
 mysql = MySQL(app)
 
-# Define the classes you want to detect
-target_classes = {'person', 'door', 'staircase'}  # Assuming 'child' is one of the target classes
-
-# Define polygonal zones as a list of vertices (x, y)
-polygon_zones = {
-    'zone1': [(100, 200), (150, 250), (200, 200), (150, 150)],
-    'zone2': [(300, 100), (350, 150), (400, 100), (350, 50)],
-    # Add more zones as needed
-}
-
 # Initialize object counts
-object_counts = {class_name: 0 for class_name in target_classes}
-object_counts.update({f'{class_name}_{zone_name}': 0 for class_name in target_classes for zone_name in polygon_zones})
+unique_objects = set()
+# Define the classes you want to detect
+target_classes = {'person', 'door', 'staircase'}
+elapsed_time = 0  # Initialize elapsed_time
 
 
-def filter_results(results):
+def filter_results(results, frame):
     filtered_results = []
 
     for result in results:
@@ -40,85 +32,153 @@ def filter_results(results):
         class_ids = result.boxes.cls
 
         filtered_boxes = []
+
         for box, confidence, class_id in zip(boxes, confidences, class_ids):
             label = model.names[int(class_id)]
-            if label in target_classes:
+
+            if label:
+                # Check if the object is unique based on its bounding box
+                box_key = tuple(box)
+                if box_key not in unique_objects:
+                    unique_objects.add(box_key)
+
+                    # Update the count in the database
+                    insert_detection_count("total", len(unique_objects))
+
+                    # Draw bounding box and label
+                    x_min, y_min, x_max, y_max = map(int, box)
+                    cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0, 255, 0), 1)
+                    label_text = f"{label}: {confidence:.2f}"
+                    cv2.putText(frame, label_text, (x_min, y_min - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+
+                # Update the count in the database
+                insert_detection_count(label, 1)
+
                 filtered_boxes.append((box, confidence, class_id))
-
-                # Increment object count for the detected class
-                object_counts[label] += 1
-
-                # Check if the object is within any polygonal zone
-                for zone_name, zone_vertices in polygon_zones.items():
-                    if is_point_in_polygon((box[0] + box[2]) / 2, (box[1] + box[3]) / 2, zone_vertices):
-                        # Increment object count for the zone
-                        object_counts[f'{label}_{zone_name}'] += 1
 
         filtered_results.append(filtered_boxes)
 
     return filtered_results
 
-def is_point_in_polygon(x, y, vertices):
-    # Check if a point is inside a polygon using the ray-casting algorithm
-    # This function can be implemented or you can use a library like Shapely
-    pass
-
 
 def detect_objects(frame):
+    global unique_objects
     results = model.track(frame, persist=True)
+    detections = filter_results(results, frame)
 
-    # Draw filtered results on the frame
-    for filtered_boxes in filter_results(results):
+    for filtered_boxes in detections:
         for box, confidence, class_id in filtered_boxes:
-            x_min, y_min, x_max, y_max = map(int, box)
+            label = model.names[int(class_id)]
+            if label in target_classes:
+                new_object = True
 
-            # Draw bounding box
-            cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
+                # Check if the bounding box is already in the set
+                if tuple(box) in unique_objects:
+                    new_object = False
 
-            # Display label and confidence
-            label = f"{model.names[int(class_id)]}: {confidence:.2f}"
-            cv2.putText(frame, label, (x_min, y_min - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                if new_object:
+                    unique_objects.add(tuple(box))
 
-    return frame
+                    # Update the count in the database
+                    insert_detection_count("total", len(unique_objects))
+
+                    # Draw bounding box and label
+                    x_min, y_min, x_max, y_max = map(int, box)
+                    cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
+                    label_text = f"{label}: {confidence:.2f}"
+                    cv2.putText(frame, label_text, (x_min, y_min - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+                    # Update the count in the database for the specific object type
+                    insert_detection_count(label, 1)
+
+    # Update the count in the database for the "total" object type
+    insert_detection_count("total", sum(len(boxes) for boxes in detections))
+
+    # Display real-time detection information on the frame
+    draw_detection_info(frame)
+
+    return frame, results
 
 
-def generate_frames():
-    while True:
-        success, frame = cap.read()
-        if not success:
-            break
-        else:
-            frame = detect_objects(frame)
+def draw_detection_info(frame):
+    # Display specific object counts and elapsed time on the frame
+    bounding_box_count = sum(len(boxes) for boxes in filter_results(model.track(frame), frame))
+    insert_detection_count("total", bounding_box_count)
 
-            # Convert object_counts to JSON
-            object_counts_json = json.dumps(object_counts)
-
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
-                   b'Content-Type: application/json\r\n\r\n' + object_counts_json.encode() + b'\r\n')
-
-
-def insert_detection(object_type, processing_time):
-    with app.app_context():
-        cursor = mysql.connection.cursor()
-        cursor.execute(
-            "INSERT INTO detection_data (object_type, processing_time) VALUES (%s, %s)",
-            (object_type, processing_time)
-        )
-        mysql.connection.commit()
-        cursor.close()
+    info_text = f"Total Bounding Boxes: {bounding_box_count}, Elapsed Time: {elapsed_time:.1f}ms"
+    cv2.putText(frame, info_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', zones=polygon_zones, classes=target_classes)
+    return render_template('index.html')  # You can add zones and classes if needed
 
 
 @app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+# ... (previous code)
+
+
+def generate_frames():
+    global elapsed_time
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+        else:
+            start_time = time.time()
+            frame, results = detect_objects(frame)
+            elapsed_time = (time.time() - start_time) * 1000  # time in milliseconds
+
+            # Convert PyTorch Tensor objects to NumPy arrays for serialization
+            filtered_results = filter_results(results, frame)
+            filtered_results_np = [
+                [
+                    (
+                        box.tolist(),  # Convert NumPy array to Python list
+                        float(confidence),
+                        int(class_id)
+                    )
+                    for box, confidence, class_id in boxes
+                ]
+                for boxes in filtered_results
+            ]
+
+            detection_data = {
+                "total_object_count": len(unique_objects),
+                "elapsed_time": elapsed_time,
+                "filtered_results": filtered_results_np
+            }
+
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (
+                b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
+                b'Content-Type: application/json\r\n\r\n' + json.dumps(detection_data).encode() + b'\r\n'
+            )
+
+# ... (remaining code)
+
+
+def overlaps_significantly(box1, box2, threshold=0.5):
+    # Implement logic to check if box1 and box2 overlap significantly
+    # For simplicity, you can use the Intersection over Union (IoU) method or any other method you prefer
+    pass
+
+
+def insert_detection_count(object_type, count):
+    with app.app_context():
+        cursor = mysql.connection.cursor()
+        cursor.execute(
+            "INSERT INTO detection_data (object_type, count) VALUES (%s, %s) ON DUPLICATE KEY UPDATE count = %s",
+            (object_type, count, count)
+        )
+        mysql.connection.commit()
+        cursor.close()
 
 
 if __name__ == '__main__':
@@ -130,9 +190,11 @@ if __name__ == '__main__':
                 CREATE TABLE IF NOT EXISTS detection_data (
                     id INT AUTO_INCREMENT PRIMARY KEY,
                     object_type VARCHAR(255) NOT NULL,
-                    processing_time FLOAT NOT NULL,
-                    detection_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                    count INT DEFAULT 0,
+                    UNIQUE KEY (object_type)
                 )
                 """
             )
+            mysql.connection.commit()
+
     app.run(debug=True)
